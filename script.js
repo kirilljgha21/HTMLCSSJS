5a3d7051916b796fb8634fa548511df5237a3d1a
kirilljgha21 <42682505+kirilljgha21@users.noreply.github.com>
2024-05-15 19:01:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// 1)
+
+let number = +prompt('Введите число');
+console.log(number ** 3);
+
+//////////////////////////////////////////////////////////
+
+
+// 2)
+
+let number_1 = +prompt('Введите первое число');
+let number_2 = +prompt('Введите второе число');
+console.log(number_1 + number_2);
+
+//////////////////////////////////////////////////////////
+
+
+// 3)
+
+let user_name = prompt('Введите ваше имя');
+let user_surname = prompt('Введите вашу фамилию');
+let user_age = +prompt('Введите ваш возвраст');
+let user_position = prompt('Введите вашу позицию');
+
+console.log(`Hi, my name is ${user_name} ${user_surname}. I was born in ${2024 - user_age}. I'm ${user_position}.`);
+
+//////////////////////////////////////////////////////////
